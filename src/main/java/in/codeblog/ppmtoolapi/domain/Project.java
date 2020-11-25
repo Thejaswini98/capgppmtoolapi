38902e412afe77fb4033bbbb86656821c303d7db
Thejaswini98 <thejaswinishreekanth98@gmail.com>
2020-11-25 11:24:33 +0530

    added documents on project

--- src/main/java/in/codeblog/ppmtoolapi/domain/Project.java
+++ src/main/java/in/codeblog/ppmtoolapi/domain/Project.java
@@ -1,3 +1,8 @@
+/**
+ * @author Thejaswini 
+ * Project domain is used as a data traveler object
+ * All project validation will be performed here
+ */
 package in.codeblog.ppmtoolapi.domain;
 
 public class Project {
